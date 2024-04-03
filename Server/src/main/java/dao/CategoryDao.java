fcf7f2ed6709c87ec118cebcdb9ed126cd3c8447
ashleynguci <hoangkinboy@gmail.com>
2024-04-03 23:18:17 +0700

    [Create dao] - [interface_Category]

---
+++ Server/src/main/java/dao/CategoryDao.java
@@ -0,0 +1,14 @@
+/*
+ * @ (#) .java   1.0    03/04/2024
+ *
+ * Copyright (c) 2024 IUH. All rights reserved.
+ */
+ 
+package dao;/*
+ * @description:
+ * @author:     Hoang Le
+ * @date:       03/04/2024
+ * @version:    1.0
+ */
+public interface CategoryDao {
+}
